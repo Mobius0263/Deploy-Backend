2d78c1d2c5ed2a8348778f2148a53a0a092e99d8
Mobius0263 <mahendra33115@gmail.com>
2025-07-07 14:30:36 +0700

    CORS update

--- config/cors.go
+++ config/cors.go
@@ -5,6 +5,7 @@ var allowedOrigins = []string{
 	"http://Mobius0263.github.io",
 	"http://localhost:5173/",
 	"http://127.0.0.1:8099/",
+	"https://deploy-frontend-henna.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
