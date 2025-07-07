9f00c6b62f3ef600cc9189f7ec8ce2dd704f4738
Mobius0263 <mahendra33115@gmail.com>
2025-07-07 13:45:43 +0700

    Frontend chage

--- frontend/src/services/authService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://mobius0263.up.railway.app/login";
-
-export const login = async (username, password) => {
-  const response = await axios.post(API_URL, { username, password });
-  return response.data;
-};
